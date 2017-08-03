6fea8c6ee0ea96baec018e84cddfe0be719052ac
Samuel Colvin <s@muelcolvin.com>
2017-08-03 14:56:50 +0100

    adding beta setup

--- start-prod.sh
+++ start-prod.sh
@@ -4,6 +4,4 @@ gnome-terminal \
     --geometry=180x40 \
     --tab-with-profile=prod \
     -x bash -c "cd $(dirname "$0"); \
-    source activate.prod.sh; \
-    docker ps --format 'table {{.ID}}\t{{.Names}}\t{{.Status}}\t{{.Ports}}\t{{.Image}}'; \
-    exec bash"
+    exec bash --rcfile ${1:-activate.prod.sh}"
