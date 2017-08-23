35cddf8d56caee368799380bdb0ca1040dec854b
Samuel Colvin <s@muelcolvin.com>
2017-08-23 12:19:45 +0100

    adding run-es.sh

---
+++ run-es.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# run elasticsearch in docker. Useful for tests or when running morpheus locally without compose
+
+docker run --rm \
+  -p 9200:9200 \
+  -e "http.host=0.0.0.0" \
+  -e "transport.host=127.0.0.1" \
+  -e "xpack.security.enabled=false" \
+  docker.elastic.co/elasticsearch/elasticsearch:5.4.2
