42318f405d988df34ecfa81665e00de201fb9a55
Alex Ford <alextford1@gmail.com>
2021-02-04 14:50:06 +0000

    comments

--- morpheus/app/worker.py
+++ morpheus/app/worker.py
@@ -529,8 +529,10 @@ class SendSMS:
             return await self._messagebird_get_mcc_cost(redis, mcc)
 
     async def _messagebird_send_sms(self, sms_data: SmsData):
-        # msg_cost = await self._messagebird_get_number_cost(sms_data.number)
-        msg_cost = 0  # Set to SMS cost to 0 until cost API is working/changed
+        try:
+            msg_cost = await self._messagebird_get_number_cost(sms_data.number)
+        except AssertionError:
+            msg_cost = 0  # Set to SMS cost to 0 until cost API is working/changed
         if msg_cost is None:
             return
 
