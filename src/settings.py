eb50231b61b6d1c47a7ac03f61142cfb911f5b23
tomhamiltonstubber <tomhamiltonstubber@gmail.com>
2021-08-17 19:07:42 +0200

    Heroku DB url

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,6 @@
 from foxglove import BaseSettings
 from pathlib import Path
-from pydantic import NoneStr
+from pydantic import NoneStr, validator
 from typing import List
 
 THIS_DIR = Path(__file__).parent.resolve()
@@ -38,6 +38,10 @@ class Settings(BaseSettings):
     # Have to use a US number as the originator to send to the US
     # https://support.messagebird.com/hc/en-us/articles/208747865-United-States
     us_send_number = '15744445663'
+
+    @validator('pg_dsn')
+    def heroku_ready_pg_dsn(cls, v):
+        return v.replace('gres://', 'gresql://')
 
     @property
     def mandrill_webhook_url(self):
